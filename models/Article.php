af5327f08bbbfd2b521abc7b533b2f3556955096
Spector9 <spector0627@gmail.com>
2018-05-15 22:07:57 +0200

    cikk lekérdezések

--- models/Article.php
+++ models/Article.php
@@ -29,4 +29,40 @@ class Article extends DB
 
     private $insertArticleSQL = "INSERT INTO cikk (nyelv, felhasznalo, lektor, cim, status, role) VALUES " .
 "('{{lang}}', '{{felhasznalo}}', '{{lektor}}', '{{cim}}', '{{status}}', '{{role}}' )";
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * Az adott felhasználó által írt cikkek lekérdezése
+     * @param $article
+     * @return array
+     */
+
+    public function getArticelByUser($article) {
+        $sql = replaceValues($this->getArticleByUserSQL, $article);
+        return $this->query($sql)->getFetchedResult();
+    }
+
+    /** Az adott lektor által validált cikkek lekérdezése
+     * @param $article
+     * @return array
+     */
+
+    public function getArticelByLektor($article) {
+        $sql = replaceValues($this->getAricleByLektorSQL, $article);
+        return $this->query($sql)->getFetchedResult();
+    }
+
+    /**
+     * Adott nyelvű cikkek lekérdezése
+     * @param $article
+     * @return array
+     */
+
+    public function getArticelByLang($article) {
+        $sql = replaceValues($this->getAricleByLangSQL, $article);
+        return $this->query($sql)->getFetchedResult();
+    }
 }
